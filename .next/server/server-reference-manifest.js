ba19f310dba6409f4d3aa877bea994ea83705cd0
José Pérez <9531447499a@gmail.com>
2025-04-20 23:20:46 -0600

    Archivos listos

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"u6AzphtHe6COOdlzEOUXGzE81sq06Zm86KRCYTQ6vMQ=\"\n}"
---
+++ src/app/products/mostrador/page.tsx
@@ -0,0 +1,28 @@
+
+export default function MostradorPage() {
+
+
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-screen">
+        <span>Productos en el carrito</span>     
+        <span className='text-7xl'>
+          22
+          </span>  
+
+          <div className="flex my-6">
+          <button
+            //onClick={() => setCount(count + 1)}
+            className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+            +1
+          </button>
+
+          <button
+            //onClick={() => setCount(count - 1)}
+            className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+            -1
+          </button>
+        </div>
+        
+      </div>
+    );
+  }
